2a41b70cd35c55ae235dfd71dc635d160855057d
Oleg Borschev <bosoleg89@gmail.com>
2019-09-25 10:46:24 +0300

    fixed user schema

--- src/modules/db/schema/user.js
+++ src/modules/db/schema/user.js
@@ -3,13 +3,25 @@ const { Schema } = mongoose;
 const timestamp = require("../middleware/timestamp");
 
 const userSchema = new Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true
+  },
   telephone: Number,
   password: String,
   email: String,
-  favoriteProducts: Array,
-  viewedProducts: Array,
-  orders: Array
+  favoriteProducts: {
+    type: Schema.Types.ObjectId,
+    ref: "Products"
+  },
+  viewedProducts: {
+    type: Schema.Types.ObjectId,
+    ref: "Products"
+  },
+  orders: {
+    type: Schema.Types.ObjectId,
+    ref: "Orders"
+  }
 });
 userSchema.plugin(timestamp);
 
